5973fb4fe391bdc8d2de596ba54f6ebf8467c14e
Natalia Polishchuk <Natalia@Natalias-MacBook-Pro.local>
2016-09-12 12:28:36 -0700

    set up inquiry form

---
+++ app/models/inquiry.rb
@@ -0,0 +1,19 @@
+class Inquiry < MailForm::Base
+  attribute :first_name,      :validate => true
+  attribute :last_name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :file,      :attachment => true
+  attribute :car_make,      :validate => true
+  attribute :car_model,      :validate => true
+  attribute :car_year,      :validate => true
+  attribute :message
+  attribute :nickname,  :captcha  => true
+
+  def headers
+    {
+      :subject => "Quote Request",
+      :to => "natapoli90@gmail.com",
+      :from => %("#{first_name} #{last_name}" <#{email}>)
+    }
+  end
+end
