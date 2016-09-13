14895f63150ae30ba87c64df4ac1fd357e101b0f
Natalia Polishchuk <Natalia@natalias-mbp.gateway.sonic.net>
2016-09-12 23:46:10 -0700

    create inquiry_mailer

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+
 gem 'mail_form', '~> 1.5'
 gem 'sendgrid-ruby'
 # Use Capistrano for deployment
--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,24 +1,26 @@
+
 class InquiriesController < ApplicationController
 
   def new
     @inquiry= Inquiry.new
-    @services = Service.new
+    @services = Service.all
   end
 
   def create
-    @inquiry= Inquiry.new(params[:inquiry])
-    @inquiry.request = request
+    @inquiry= Inquiry.new(params[:id])
+
     @service = Service.find_by(name: params[:name])
     @companies = @service.companies
-    @emails = []
-    @companies.each do |co|
-      @emails << co.email
-    end
-    @inquiry.emails = @emails
+    # @emails = []
+    # @companies.each do |co|
+    #   @emails << co.email
+    # end
+    # @inquiry.emails = @emails
     @inquiry.name = @service.name
     @inquiry.category = @service.category
-    if @inquiry.deliver
-      flash[:error] = nil
+
+    if @inquiry.send_email(@inquiry, @service).deliver
+      flash[:error] = "Message delivered"
     else
       flash[:error] = "There was en error. #{@inquiry.errors.full_messages.join(', ')}"
       render :new
