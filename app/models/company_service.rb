a7162e03e783bb7d1030139d6a00eb493831f6ed
Natalia Polishchuk <Natalia@natalias-mbp.gateway.sonic.net>
2016-09-28 19:26:12 -0700

    fix index and create gor company services, add seed data for services

--- app/models/company_service.rb
+++ app/models/company_service.rb
@@ -3,5 +3,4 @@ class CompanyService < ApplicationRecord
   belongs_to :company
 
   Category = ["Under the hood", "Under the car", "Exterior and Perfomance"];
-  Name = ["Services 1", "Services 2", "Services 3"];
 end
